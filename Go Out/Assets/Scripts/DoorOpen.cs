2d21c3688c6e581722e196fee55f6030d4e670ad
unknown <Development@GSD-VR03.vtc.edu.hk>
2023-11-20 16:56:34 +0800

    add room

---
+++ Go Out/Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DoorOpen : MonoBehaviour
+{
+
+    private GameObject GB;
+    [SerializeField]
+    private bool ispress = false;
+    [SerializeField]
+    private Vector3 targetPosition;
+
+    private float speed = 5.0f;
+
+    private void Update()
+    {
+        if (ispress)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        }
+
+       
+           // transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        
+    }
+
+    public void press()
+    {
+        ispress = !ispress;
+    }
+}
