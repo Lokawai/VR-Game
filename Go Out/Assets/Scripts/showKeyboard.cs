fb091c49acb707c595849339df98e2b9c931cb67
Ko Chi Hong <93576351+hitwuwuwu@users.noreply.github.com>
2023-12-01 11:34:01 +0800

    updata the show keyboard script

--- Go Out/Assets/Scripts/showKeyboard.cs
+++ Go Out/Assets/Scripts/showKeyboard.cs
@@ -7,6 +7,10 @@ using Microsoft.MixedReality.Toolkit.Experimental.UI;
 public class showKeyboard : MonoBehaviour
 {
     private TMP_InputField inputField;
+    public float distance = 0.5f;
+    public float verticaloffset = -0.5f;
+
+    public Transform positionSource;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +22,29 @@ public class showKeyboard : MonoBehaviour
     {
         NonNativeKeyboard.Instance.InputField = inputField;
         NonNativeKeyboard.Instance.PresentKeyboard(inputField.text);
+
+        Vector3 direction = positionSource.forward;
+        direction.y = 0;
+        direction.Normalize();
+
+        Vector3 targetPosition = positionSource.position + direction * distance + Vector3.up * verticaloffset;
+
+        NonNativeKeyboard.Instance.RepositionKeyboard(targetPosition);
+
+        SetCaretColorAlpha(1);
+        NonNativeKeyboard.Instance.OnClosed += Instance_OnClosed;
+
+    }
+
+    private void Instance_OnClosed(object sender, System.EventArgs e)
+    {
+        SetCaretColorAlpha(0);
+    } 
+    private void SetCaretColorAlpha(float value)
+    {
+        inputField.customCaretColor = true;
+        Color caretColor = inputField.caretColor;
+        caretColor.a = value;
+        inputField.caretColor = caretColor;
     }
 }
